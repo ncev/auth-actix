bac35b8c9fcb30cb163f680d2f5d3435bef91c59
Nicolas <nicolaseven76@gmail.com>
2021-01-01 15:55:31 +0100

    moving orphan instances

--- src/types.rs
+++ src/types.rs
@@ -2,11 +2,15 @@ use std::fmt::{Display, Formatter};
 use std::fmt;
 use serde::de::DeserializeOwned;
 use serde::Deserialize;
-use actix_web::Error;
+use actix_web::{Error, ResponseError};
+use actix_web::http::StatusCode;
 
 
 /// AuthResult => type alias
 pub type AuthResult<T> = Result<Auth<T>, Error>;
+
+
+
 
 
 
@@ -19,11 +23,12 @@ pub struct AuthConfiguration {
 
 
 
+
+
 /// idiom type to wrap the type identification
 pub struct Auth<T: DeserializeOwned + Display> {
     pub(crate) wrapped: T
 }
-
 
 
 /// unwrap the Auth value
@@ -33,7 +38,6 @@ pub fn run_auth<T>(auth: Auth<T>) -> T
     { auth.wrapped }
 
 
-
 /// Display implementation, needed by FromRequest
 impl<T: DeserializeOwned + Display> Display for Auth<T> {
     fn fmt(&self, f: &mut Formatter<'_>)
@@ -41,9 +45,32 @@ impl<T: DeserializeOwned + Display> Display for Auth<T> {
 }
 
 
+
+
+
 /// Authentication failed representation
 #[derive(Debug)]
 pub enum AuthenticationError {
     Failed,
     MissingConfiguration
+}
+
+/// display implementation, needed for FromRequest trait
+impl fmt::Display for AuthenticationError {
+    fn fmt(&self, f: &mut Formatter<'_>)
+           -> fmt::Result {
+        match self {
+            AuthenticationError::Failed =>
+                write!(f, "Missing configuration, inject it through app_data"),
+            AuthenticationError::MissingConfiguration =>
+                write!(f, "Authentication failed")
+        }
+    }
+}
+
+
+/// ResponseError implementation, UNAUTHORIZED being 401
+impl ResponseError for AuthenticationError {
+    fn status_code(&self)
+                   -> StatusCode { StatusCode::UNAUTHORIZED }
 }
